55ff1ad54a10f71ec220db77d716b505f97062e7
oya riku <56904584+rikurr@users.noreply.github.com>
2023-05-05 20:07:12 +0900

    add connection postgres

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::net::TcpListener;
+use std::{fmt::format, net::TcpListener};
 
-use zero2prod::run;
+use zero2prod::{configuration::get_configuration, run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind address");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
 
     run(listener)?.await
 }
